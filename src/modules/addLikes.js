ac02def8f896bc9206c76fa566178e2d2244efdc
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-05-30 15:26:59 +0100

    Add the AddLikes function

--- src/modules/addLikes.js
+++ src/modules/addLikes.js
@@ -9,3 +9,16 @@ export const updateLikes = async (key, likes) => {
     return '';
   });
 };
+
+const addLikes = async (key, likes) => {
+  const url = `http://localhost:3000/posts/${key}`;
+  await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: '${key}',
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
