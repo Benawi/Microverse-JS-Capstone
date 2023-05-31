330c4b6e27414638e4bcec15db62956b25ce12dc
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-31 14:09:47 +0300

    Add fetched data from specified api

---
+++ src/modules/addRapidApi.js
@@ -0,0 +1,32 @@
+import commentsPopUp from './addComments.js';
+import renderCards from './displayItems.js';
+import { url, options } from './api.js';
+
+const getShazamTracks = async () => {
+  if (localStorage.getItem('songs')) {
+    const localStorageItem = localStorage.getItem('songs');
+    const songs = JSON.parse(localStorageItem);
+
+    renderCards(songs);
+    commentsPopUp();
+
+  } else {
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      if (result && result.tracks) {
+        const songs = result.tracks;
+        localStorage.setItem('songs', JSON.stringify(songs));
+
+        renderCards(songs);
+        commentsPopUp();
+   
+      } else {
+        throw new Error('Failed to get Shazam tracks');
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+};
+export default getShazamTracks;
