599881c72deffc1097d9ef79f5b6de4bf12dfda2
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-05-31 11:10:25 +0100

    Fix linters bug

--- src/modules/displayItems.js
+++ src/modules/displayItems.js
@@ -1,5 +1,5 @@
-import { addLike, fetchLikes } from './addLike.js';
-import itemsCounter from './itemCounter.js';
+import { addLike, fetchLikes } from './addLikes.js';
+import itemsCounter from './addItemCounter.js';
 
 const cards = document.getElementById('cards');
 
@@ -8,19 +8,19 @@ const renderCards = (array) => {
   array.forEach((data) => {
     const card = `
   <div id="${
-    data.key
-  }" class="flex flex-col gap-2 p-4 h-full rounded-2xl bg-[#1C1F26] drop-shadow-md border-2 border-[#a8b3cf33;] hover:border-[#1ED760;] card-color">
+  data.key
+}" class="flex flex-col gap-2 p-4 h-full rounded-2xl bg-[#1C1F26] drop-shadow-md border-2 border-[#a8b3cf33;] hover:border-[#1ED760;] card-color">
   <div class="w-full h-[250px] rounded-lg">
   <img class="h-[250px] w-auto rounded-lg mx-auto" src="${
-    data.images?.coverart ||
-    data.images?.background ||
-    `https://dummyimage.com/400x400/000/1aff00&text=${data.title}`
-  }" />
+  data.images?.coverart
+    || data.images?.background
+    || `https://dummyimage.com/400x400/000/1aff00&text=${data.title}`
+}" />
   </div>
   <div class="flex flex-col gap-2 justify-between">
   <h2 class="font-semibold text-lg capitalize custom-title">${
-    data.title.length < 28 ? data.title : `${data.title.substring(0, 25)}...`
-  }</h2>
+  data.title.length < 28 ? data.title : `${data.title.substring(0, 25)}...`
+}</h2>
   <h5 class="text-[#777] -mt-2"></h5>
   <div class="flex flex-col gap-1 justify-between">
   <div class="flex justify-between p-1 items-center">
