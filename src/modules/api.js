6098303652939ae9c9eee77ee7c7aeeb86c0ec26
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-31 13:52:08 +0300

    Add api for source of data

---
+++ src/modules/api.js
@@ -0,0 +1,8 @@
+export const url = 'https://spotify81.p.rapidapi.com/top_20_by_monthly_listeners';
+export const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'a8586c69f6msh245507aaa238195p14a689jsn95bb08bc46cd',
+		'X-RapidAPI-Host': 'spotify81.p.rapidapi.com',
+  },
+};
