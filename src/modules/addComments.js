3bfc9c74bcc948de133e8dcb267ad963fad91fa0
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-31 11:24:01 +0300

    Add add method to add comments

--- src/modules/addComments.js
+++ src/modules/addComments.js
@@ -9,3 +9,16 @@ const getComments = async (itemId) => {
       throw new Error(error);
     }
   };
+
+  const addComment = async (itemId, name, comment) => {
+    await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/6b6MwShRJVij7XaDVDix/comments', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        item_id: itemId,
+        username: name,
+        comment,
+      }),
+    });
+  };
+  
